464a3b06960065e2a247097743824feb520158c1
gunasekaran1710 <guna171003@gmail.com>
2025-05-13 11:28:47 +0530

    notify

--- flashapp/views.py
+++ flashapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import FlashSale
 from django.utils import timezone
 
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -9,10 +10,20 @@ def login_view(request):
         return redirect('flashsales')
     return render(request, 'login.html')
 
+# def flashsales_view(request):
+#     if 'username' not in request.session:
+#         return redirect('login')
+
+#     now = timezone.now()
+#     sales = FlashSale.objects.filter(start_time__lte=now, end_time__gte=now)
+#     return render(request, 'flashsales.html', {'sales': sales, 'username': request.session['username']})
 def flashsales_view(request):
     if 'username' not in request.session:
         return redirect('login')
 
     now = timezone.now()
     sales = FlashSale.objects.filter(start_time__lte=now, end_time__gte=now)
-    return render(request, 'flashsales.html', {'sales': sales, 'username': request.session['username']})
+    return render(request, 'flashsales.html', {
+        'sales': sales,
+        'username': request.session['username']
+    })

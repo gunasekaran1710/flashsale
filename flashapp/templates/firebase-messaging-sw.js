343168d36059a69feafaaf3c5ba2e8f02ccc97bd
gunasekaran1710 <guna171003@gmail.com>
2025-05-13 12:59:32 +0530

    modify

--- flashapp/templates/firebase-messaging-sw.js
+++ flashapp/templates/firebase-messaging-sw.js
@@ -15,6 +15,7 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
+  
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {

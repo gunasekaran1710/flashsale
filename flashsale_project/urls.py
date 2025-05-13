e262307fff44c6f67b8e9de245efffc844cdaa0b
gunasekaran1710 <guna171003@gmail.com>
2025-05-13 13:14:16 +0530

    notify

--- flashsale_project/urls.py
+++ flashsale_project/urls.py
@@ -21,3 +21,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('flashapp.urls')),
 ]
+
+
+from django.views.generic import TemplateView
+from django.urls import path
+
+urlpatterns += [
+    path('firebase-messaging-sw.js', TemplateView.as_view(
+        template_name="firebase-messaging-sw.js",
+        content_type='application/javascript',
+    ), name='firebase-sw')
+]
+

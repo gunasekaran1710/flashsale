6b3847fe839527f54af90f9ad7cca3cc2b0df1a8
gunasekaran1710 <guna171003@gmail.com>
2025-05-13 12:32:22 +0530

    notify

---
+++ flashapp/templates/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtrJeMwMfj_rXWJe9000x2845S7Y1smCM",
+  authDomain: "gunasekaran-80514.firebaseapp.com",
+  projectId: "gunasekaran-80514",
+  storageBucket: "gunasekaran-80514.appspot.com",
+  messagingSenderId: "614767996940",
+  appId: "1:614767996940:web:6f3b4106f60ece430d3e29",
+  measurementId: "G-GNLVP9K3VX"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
--- flashapp/urls.py
+++ flashapp/urls.py
@@ -1,7 +1,21 @@
-from django.urls import path
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.login_view, name='login'),
+#     path('flashsales/', views.flashsales_view, name='flashsales'),
+# ]
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('flashsales/', views.flashsales_view, name='flashsales'),
+
+    # Serve the Firebase service worker from root
+    re_path(r'^firebase-messaging-sw.js$', TemplateView.as_view(
+        template_name="firebase-messaging-sw.js",  # this must be placed in the templates folder
+        content_type='application/javascript',
+    )),
 ]
